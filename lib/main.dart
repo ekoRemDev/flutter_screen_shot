69a92837eeab162b07128c8407da3ee3cdd4f053
Eko <ekoremdev@gmail.com>
2020-04-01 01:36:39 +0400

    screen shot

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:image_picker_saver/image_picker_saver.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -21,45 +28,63 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static GlobalKey screen = new GlobalKey();
 
-
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    //RepaintBoundary
+    return new Scaffold(
+      appBar: new AppBar(
+
+        title: new Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: RepaintBoundary(
+        key: screen,
+        child:Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              new Text(
+                'click the button below to capture image',
+              ),
+              new RaisedButton(
+                child: Text('capture Image'),
+                onPressed: (){},
+              ),
+              Image.network("https://images.pexels.com/photos/462118/pexels-photo-462118.jpeg",height: 200,width: 200,)
+
+            ],
+
+          ),
+
+        ) ,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+
+
+
+      floatingActionButton: new FloatingActionButton(
+        onPressed: ScreenShot,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: new Icon(Icons.add),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+  }
+  ScreenShot() async{
+    RenderRepaintBoundary boundary = screen.currentContext.findRenderObject();
+    ui.Image image = await boundary.toImage();
+    ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    var filePath = await ImagePickerSaver.saveFile(fileData:byteData.buffer.asUint8List() );
+    print(filePath);
+
   }
 }
